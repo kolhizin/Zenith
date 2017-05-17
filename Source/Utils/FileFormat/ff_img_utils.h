646eea73e9cc5c84a0cbb87ffb166aec5e4b5bb3
kolhizin <kolhizin@gmail.com>
2017-05-17 21:04:53 +0300

    Updated zIMG format and tool for conversion from PNG

---
+++ Source/Utils/FileFormat/ff_img_utils.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "ff_img.h"
+namespace zenith
+{
+	namespace util
+	{
+		namespace zfile_format
+		{
+			zImgDescription zimg_from_png(void * src, uint64_t size);
+			zImgDescription zimg_from_img(void * src, uint64_t size, const char * fmt);
+			zImgDescription zimg_change_format(const zImgDescription &src, ImageFormat dstFormat);
+			void zimg_free(zImgDescription &d);
+		}
+	}
+}
